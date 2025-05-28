956a3cb51f4407f0ec4065b71c0d63e1e563927b
Shivam Yadav <shivamyadav69998@gmail.com>
2025-05-28 14:23:32 +0530

    email validation

--- medicure/settings.py
+++ medicure/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 from datetime import timedelta
 from dotenv import load_dotenv
 import os
-import dj_database_url
+
 
 load_dotenv()
 
@@ -138,8 +138,12 @@ WSGI_APPLICATION = 'medicure.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
 
 
 
